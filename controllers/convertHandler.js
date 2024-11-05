c36f4bc9cdd4ae34b748ab48ea2878a36a339d53
Cepri Damiri <damiricepri127@gmail.com>
2024-11-05 07:32:09 +0000

    Adding fungsionality and fungsionaltesting

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,42 +1,145 @@
+// fungsi untuk valiadsi input number
+function numberStringSpilter(input){
+  let number = input.match(/[.\d\/]+/g) || ["1"];
+  let string = input.match(/[a-zA-Z]+/g)[0];
+
+  return [number[0], string];
+}
+
+// fungsi untuk melihat hasil bagi
+function cekDiv(possibleFraction){
+  let nums = possibleFraction.split("/");
+  if(nums.length > 2){
+    return false;
+  }
+
+  return nums;
+}
+
+
+
 function ConvertHandler() {
   
+  // fungsi mendapatkan number
   this.getNum = function(input) {
-    let result;
-    
+    let result = numberStringSpilter(input)[0];
+    let nums = cekDiv(result);
+    if (!nums){
+      return undefined;
+    }
+
+    let number1 = nums[0];
+    let number2 = nums[1] || "1";
+    result = parseFloat(number1) / parseFloat(number2);
+    if (isNaN(number1) || isNaN(number2)){
+      return undefined;
+    }
     return result;
   };
   
+  // fungsi untuk mendapatkan unit
   this.getUnit = function(input) {
-    let result;
-    
-    return result;
+    let result = numberStringSpilter(input)[1].toLowerCase();
+    switch(result){
+      case 'km':
+        return "km";
+      case 'gal':
+        return "gal";
+      case 'lbs':
+        return "lbs";
+      case "mi":
+        return "mi";
+      case 'l':
+        return "L";
+      case 'kg':
+        return "kg";
+      default:
+        return undefined;
+    }
   };
   
+  // fungsi untuk mendapatkan nilai unit ketika diconvert
   this.getReturnUnit = function(initUnit) {
-    let result;
+    let unit = initUnit.toLowerCase();
     
-    return result;
+    switch(unit){
+      case 'km':
+        return "mi";
+      case 'gal':
+        return "L";
+      case 'lbs':
+        return "kg";
+      case 'mi':
+        return "km";
+      case 'l':
+        return "gal";
+      case 'kg':
+        return "lbs";
+      default:
+        return undefined;
+    }
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  // fungsi untuk menjelaskan singkatan unit
+  this.spellOutUnit = function(initUnit) {
+    let unit = initUnit.toLowerCase();
+    switch(unit){
+      case 'km':
+        return "kilometer";
+      case 'gal':
+        return "gallons";
+      case 'mi':
+        return "miles";
+      case 'lbs':
+        return "pounds";
+      case 'kg':
+        return "kilograms";
+      case 'l':
+        return "liters";
+      default:
+        return "have no idea";
+    }
   };
   
+  // fungsi perhitungan (converter)
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
+    let unit = initUnit.toLowerCase();
     let result;
     
-    return result;
+    switch(unit){
+      case 'km':
+        result = initNum / miToKm;
+        break;
+      case 'gal':
+        result = initNum * galToL;
+        break;
+      case 'lbs':
+        result = initNum * lbsToKg;
+        break;
+      case 'mi':
+        result = initNum * miToKm;
+        break;
+      case 'l':
+        result = initNum / galToL;
+        break;
+      case 'kg':
+        result = initNum / lbsToKg;
+        break;
+      default:
+        result = undefined;
+    }
+    return parseFloat(result.toFixed(5));
   };
   
+  // fungsi mengembalikan hasil akhir perhitungan
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
     
-    return result;
+    return `${initNum} ${this.spellOutUnit(
+      initUnit
+    )} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
   };
   
 }
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,68 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+    suite('Routing Test', function(){
+        suite('GET /api/convert => conversion object', function(){
+            test('Convert valid input (10L)', function(){
+                chai
+                    .request(server)
+                    .get('/api/convert')
+                    .query({input: "10L"})
+                    .end(function(err, res){
+                        assert.equal(res.status, 200);
+                        assert.equal(res.body.initNum, 10);
+                        assert.equal(res.body.initUnit, 'L');
+                        assert.approximately(res.body.returnNum, 2.64172, 0.1);
+                        assert.equal(res.body.returnUnit, "gal");
+                    })
+            });
 
+            test('Convert invalid input (32g)', function(){
+                chai
+                    .request(server)
+                    .get('/api/convert')
+                    .query({input: '32g'})
+                    .end(function(err, res){
+                        assert.equal(res.status, 200);
+                        assert.equal(res.body.initUnit, undefined);
+                    })
+            });
+
+            test('Convert invalid number (3/7.2/4kg)', function(){
+                chai
+                    .request(server)
+                    .get('/api/convert')
+                    .query({input: "3/7.2/4kg"})
+                    .end(function(err, res){
+                        assert.equal(res.status, 200);
+                        assert.equal(res.body.initNum, undefined);
+                    })
+            });
+
+            test('Convert invalid number AND unit (3/7.2/4kilomegagram)', function(){
+                chai
+                    .request(server)
+                    .get('/api/convert')
+                    .query({input: "3/7.2/4kilomegagram"})
+                    .end(function(err, res){
+                        assert.equal(res.status, 200);
+                        assert.equal(res.body.initNum, undefined);
+                        assert.equal(res.body.initUnit, undefined);
+                    })
+            });
+
+            test('Convert with no number (kg)', function(){
+                chai
+                    .request(server)
+                    .get('/api/convert')
+                    .query({input: "kg"})
+                    .end(function(err, res){
+                        assert.equal(res.status, 200);
+                        assert.equal(res.body.initUnit, "kg");
+                        assert.approximately(res.body.returnNum, 2.20462, 0.1);
+                        assert.equal(res.body.returnUnit, "lbs");
+                    })
+            });
+        });
+    });
 });
